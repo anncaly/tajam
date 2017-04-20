855be04f71eeb493a7f9f58e7e8f95cf773682d6
[Ann Caly] <[anncaly@gmail.com]>
2017-04-20 23:30:43 +0300

    create works, reviews sections

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,11 @@
 // @import "js/owl.carousel.min.js";
 
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
+  $('.slide-one').owlCarousel({
   	loop: true,
   	dots: true,
   	autoplay: true,
-  	autoplayTimeout: 5000,
+  	autoplayTimeout: 4000,
   	responsive: {
   		0: {
   			items: 1, 
@@ -13,6 +13,27 @@ $(document).ready(function(){
   		}
   	}
   });
+  $('.slide-two').owlCarousel({
+  	loop: true,
+  	dots: true,
+  	autoplay: true,
+  	autoplayTimeout: 4000,
+  	dotsContainer: '#carousel-custom-dots',
+  	nav: true,
+		navText: [
+   		"<img src=\"../img/left-arrow.png\">",
+   		"<img src=\"../img/right-arrow.png\">"
+			],
+  	responsive: {
+  		0: {
+  			items: 1, 
+  			// nav: true
+  		}
+  	}
+  });
+//   $('.owl-custom-dot').click(function () {
+//     owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+// });
   $(function() {
 		$('.border').matchHeight();
 	});
